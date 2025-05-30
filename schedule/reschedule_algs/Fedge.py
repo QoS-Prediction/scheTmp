6623f26ea4ce02270ee7289fb7cdb404bee7ba1a
aweftr <aweftger@gmail.com>
2025-05-30 10:16:58 +0800

    0530 fedge modify

--- schedule/reschedule_algs/Fedge.py
+++ schedule/reschedule_algs/Fedge.py
@@ -5,3 +5,13 @@
 3. 欣阳对比所有主机的预测结果，进行调度主机判断
 
 vscode 如何创建文件夹考虑rc中的umask
+
+class FEDGE:
+    def __init__(self, cluster: Cluster):
+        self.cluster = cluster
+    
+    def _run(self, vm:detected_vm, pms: list[PM], vm_cpu_utilz: dict[VM, float], org_allocation: dict[PM, list[VM]]) -> dict[PM, list[VM]]:
+        
+        for pm in pms:
+            max_qos_pm
+            fedge(vm,pm,dist[pm])     
